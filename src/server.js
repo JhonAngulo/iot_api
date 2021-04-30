0be66898a44de8c5d1e4229a538f443739629967
Jhon Angulo <jhon.angulo@jikkosoft.com>
2021-04-30 14:49:09 -0500

    add cors y helmet

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 'use strict'
 
+const cors = require('fastify-cors')
+const helmet = require('fastify-helmet')
 const { app, logger } = require('../config/appConfig')
-
 const fastify = require('fastify')({
   logger
 })
@@ -9,10 +10,18 @@ const fastify = require('fastify')({
 const autoLoad = require('fastify-autoload')
 const path = require('path')
 
+fastify.register(cors, {})
+
 fastify.register(autoLoad, {
   dir: path.join(__dirname, 'routes/v1'),
   options: { prefix: '/api/v1' }
 })
+
+fastify.register(
+  helmet,
+  // Example disables the `contentSecurityPolicy` middleware but keeps the rest.
+  { contentSecurityPolicy: false }
+)
 
 const start = async () => {
   try {
