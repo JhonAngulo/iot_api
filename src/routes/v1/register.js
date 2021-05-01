34d6eb7e6cf9c49cbd1fb92490e3c07b698be16f
Jhon Angulo <jhon.angulo@jikkosoft.com>
2021-04-30 20:17:20 -0500

    add linter

--- src/routes/v1/register.js
+++ src/routes/v1/register.js
@@ -1,16 +1,13 @@
 'use strict'
 
 module.exports = async function (fastify, _opts) {
+  fastify.post('/', async (request, reply) => {
+    return reply.send({ data: request.body })
+  })
+
   fastify.get('/', async (_request, reply) => {
     return reply.send({ register: 'Ok' })
   })
 }
-
-module.exports = async function (fastify, _opts) {
-  fastify.post('/', async (request, reply) => {
-    return reply.send({ data: request.body })
-  })
-}
-
 
 module.exports.autoPrefix = '/register'
